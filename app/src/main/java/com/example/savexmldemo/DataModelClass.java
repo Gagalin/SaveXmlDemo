05c3e64a322c17421edbae3da6cf532085f14822
JacksonGGG <13398636561@163.com>
2020-02-19 17:43:50 +0800

    Initial commit

---
+++ app/src/main/java/com/example/savexmldemo/DataModelClass.java
@@ -0,0 +1,16 @@
+package com.example.savexmldemo;
+
+/**
+ * @author glsite.com
+ * @version Test Key$
+ * @des No
+ * @updateAuthor JJL$
+ * @updateDes No
+ */
+public class DataModelClass {
+    public int getId() {
+        return Id;
+    }
+
+    private int Id;
+}
